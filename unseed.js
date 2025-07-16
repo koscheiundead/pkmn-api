aa7076b55fd305b24d6f43ed7c8a3c042b0025d1
koscheiundead <102054008+koscheiundead@users.noreply.github.com>
2025-07-16 11:54:24 -0700

    error handling in database -> json function

--- unseed.js
+++ unseed.js
@@ -1,5 +1,4 @@
 const sqlite3 = require('sqlite3');
-const papa = require('papaparse');
 const fs = require('fs');
 
 const db = new sqlite3.Database('./database.db');
@@ -8,6 +7,12 @@ db.all('SELECT * FROM Pokemon', (err, rows) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFileSync('./out.json', JSON.stringify(rows));
+    fs.writeFileSync('./out.json', JSON.stringify(rows), 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('File written!');
+      }
+    });
   }
 });
